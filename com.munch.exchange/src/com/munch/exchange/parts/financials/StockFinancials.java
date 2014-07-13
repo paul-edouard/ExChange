ee9840376cce224a5ffd97a463f293db2d7ae38a
paul-edouard <paul-edouard@Sirka>
2014-07-13 21:23:43 +0200

    fix

--- com.munch.exchange/src/com/munch/exchange/parts/financials/StockFinancials.java
+++ com.munch.exchange/src/com/munch/exchange/parts/financials/StockFinancials.java
@@ -1,6 +1,7 @@
 package com.munch.exchange.parts.financials;
 
 import java.util.Calendar;
+import java.util.LinkedList;
 
 import javax.inject.Inject;
 
@@ -35,6 +36,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Combo;
 
 public class StockFinancials extends Composite {
 	
@@ -51,6 +53,13 @@ public class StockFinancials extends Composite {
 	private TreeViewer treeViewer;
 	private Tree tree;
 	private Button btnAddColumn;
+	private Button btnQuaterly;
+	private Button btnAnnualy;
+	private LinkedList<TreeViewerColumn> columns=new LinkedList<TreeViewerColumn>();
+	private Label lblUnit;
+	private Combo comboUnit;
+	
+	private double unitFactor=1;
 	
 	@Inject
 	public StockFinancials(Composite parent,ExchangeRate rate) {
@@ -59,8 +68,65 @@ public class StockFinancials extends Composite {
 		setLayout(new GridLayout(1, false));
 		
 		Composite compositeHeader = new Composite(this, SWT.NONE);
-		compositeHeader.setLayout(new GridLayout(1, false));
+		compositeHeader.setLayout(new GridLayout(5, false));
 		compositeHeader.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		btnQuaterly = new Button(compositeHeader, SWT.RADIO);
+		btnQuaterly.setEnabled(false);
+		btnQuaterly.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(btnQuaterly.getSelection()){
+					btnAnnualy.setSelection(false);
+					btnQuaterly.setEnabled(false);
+					btnAnnualy.setEnabled(true);
+					refreshColumns();
+				}
+			}
+		});
+		btnQuaterly.setSelection(true);
+		btnQuaterly.setText("quaterly");
+		
+		btnAnnualy = new Button(compositeHeader, SWT.RADIO);
+		btnAnnualy.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(btnAnnualy.getSelection()){
+					btnQuaterly.setSelection(false);
+					btnAnnualy.setEnabled(false);
+					btnQuaterly.setEnabled(true);
+					refreshColumns();
+				}
+			}
+		});
+		btnAnnualy.setText("annualy");
+		
+		lblUnit = new Label(compositeHeader, SWT.NONE);
+		lblUnit.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblUnit.setText("Unit:");
+		
+		comboUnit = new Combo(compositeHeader, SWT.NONE);
+		comboUnit.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(comboUnit.getText().equals("1"))
+					unitFactor=1;
+				else if(comboUnit.getText().equals("1.000")){
+					unitFactor=1000;
+				}
+				else if(comboUnit.getText().equals("1.000.000")){
+					unitFactor=1000000;
+				}
+				
+				treeViewer.refresh();
+				
+			}
+		});
+		comboUnit.add("1");
+		comboUnit.add("1.000");
+		comboUnit.add("1.000.000");
+		comboUnit.setText("1.000.000");
+		unitFactor=1000000;
 		
 		btnAddColumn = new Button(compositeHeader, SWT.NONE);
 		btnAddColumn.addSelectionListener(new SelectionAdapter() {
@@ -79,28 +145,47 @@ public class StockFinancials extends Composite {
 		tree.setHeaderVisible(true);
 		tree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
-		
+		//Main Column
+		TreeViewerColumn mainColumn = new TreeViewerColumn(treeViewer, SWT.NONE);
+		mainColumn.setLabelProvider(new mainColumnLabelProvider());
+		TreeColumn trclmnName = mainColumn.getColumn();
+		trclmnName.setWidth(300);
+		trclmnName.setText("Items");
 		
 		treeViewer.refresh();
 		
 	}
 	
 	
+	private void refreshColumns(){
+		removeColumns();
+		createColumns();
+		treeViewer.refresh();
+	}
+	
+	private void removeColumns(){
+		for(TreeViewerColumn column:columns){
+			column.getColumn().dispose();
+		}
+		columns.clear();
+	}
+	
 	private void createColumns(){
 		
-		TreeViewerColumn mainColumn = new TreeViewerColumn(treeViewer, SWT.NONE);
-		mainColumn.setLabelProvider(new mainColumnLabelProvider());
-		TreeColumn trclmnName = mainColumn.getColumn();
-		trclmnName.setWidth(300);
-		trclmnName.setText("Items");
+		String modus=FinancialPoint.PeriodeTypeQuaterly;
+		if(btnAnnualy.getSelection())
+			modus=FinancialPoint.PeriodeTypeAnnual;
+		
 		//trclmnName.addSelectionListener(getSelectionAdapter(trclmnName, 0));
 		
-		for(Calendar date :this.stock.getFinancials().getDateList(FinancialPoint.PeriodeTypeQuaterly)){
+		for(Calendar date :this.stock.getFinancials().getDateList(modus)){
 			TreeViewerColumn dateColumn = new TreeViewerColumn(treeViewer, SWT.NONE);
 			dateColumn.setLabelProvider(new valueColumnLabelProvider(date));
 			TreeColumn trclmn = dateColumn.getColumn();
 			trclmn.setWidth(150);
 			trclmn.setText(DateTool.dateToDayString(date));
+			
+			columns.add(dateColumn);
 			
 		}
 		
@@ -141,17 +226,42 @@ public class StockFinancials extends Composite {
 		}
 		
 		public String getText(Object element) {
+			
+			String modus=FinancialPoint.PeriodeTypeQuaterly;
+			if(btnAnnualy.getSelection())
+				modus=FinancialPoint.PeriodeTypeAnnual;
+			
+			
 			if(element instanceof FinancialElement){
 				FinancialElement entity=(FinancialElement) element;
-				//System.out.println("Element: "+entity+"Key: "+entity.fieldKey);
-				long val=stock.getFinancials().getValue(FinancialPoint.PeriodeTypeQuaterly,date, entity.fieldKey,entity.sectorKey);
+				long val=stock.getFinancials().getValue(modus,date, entity.fieldKey,entity.sectorKey);
 				if(val==0)return "-";
-				return FinancialPoint.LongValueToString(val);
+				return getStringOfValue(val);
 			}
 			return element == null ? "" : element.toString();
 		}
+		
+		
+		private String getStringOfValue(long value){
+			
+			if(value==Long.MIN_VALUE)return "";
+			
+			if(unitFactor==1){
+				return String.valueOf(value);
+			}
+			else if(unitFactor==1000){
+				return String.valueOf((long) (value/unitFactor));
+			}
+			else if(unitFactor==1000000){
+				//return String.format("%.1f",  ((double)value)/unitFactor);
+				return String.valueOf((long) (value/unitFactor));
+			}
+			return "-";
+			
+		}
+		
+		
 	}
-	
 	
 	
 	
@@ -175,7 +285,7 @@ public class StockFinancials extends Composite {
 			@Optional @UIEventTopic(IEventConstant.FINANCIAL_DATA_LOADED) String rate_uuid) {
 
 		if(!isCompositeAbleToReact(rate_uuid))return;
-		
+		/*
 		HistoricalIncomeStatement his=stock.getFinancials().getIncomeStatement().getQuaterlyPoints();
 		for(DatePoint point:his){
 			if(point instanceof IncomeStatementPoint){
@@ -183,7 +293,7 @@ public class StockFinancials extends Composite {
 				System.out.println(isp);
 			}
 		}
-		
+		*/
 		/*
 		HistoricalCashFlow his=stock.getFinancials().getCashFlow().getQuaterlyPoints();
 		for(DatePoint point:his){
@@ -193,8 +303,6 @@ public class StockFinancials extends Composite {
 			}
 		}
 		*/
-		
-		
 		
 		createColumns();
 		
