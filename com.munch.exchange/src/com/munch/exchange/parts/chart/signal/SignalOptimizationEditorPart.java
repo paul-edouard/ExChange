4522680021892dd4759db9107cc8a0ce7c39bf18
paul-edouard <paul-edouard@Sirka>
2015-12-03 21:14:29 +0100

    Continue of signal optimization editor part

--- com.munch.exchange/src/com/munch/exchange/parts/chart/signal/SignalOptimizationEditorPart.java
+++ com.munch.exchange/src/com/munch/exchange/parts/chart/signal/SignalOptimizationEditorPart.java
@@ -1,12 +1,18 @@
 package com.munch.exchange.parts.chart.signal;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import org.apache.log4j.Logger;
 import org.eclipse.e4.ui.di.Focus;
@@ -23,7 +29,11 @@ import org.eclipse.swt.widgets.Text;
 import com.munch.exchange.model.core.ib.bar.IbBar;
 import com.munch.exchange.model.core.ib.bar.IbBarContainer;
 import com.munch.exchange.model.core.ib.chart.signals.IbChartSignal;
+import com.munch.exchange.model.core.ib.chart.signals.IbChartSignalOptimizationController;
+import com.munch.exchange.model.core.ib.chart.signals.IbChartSignalOptimizationControllerEvent;
+import com.munch.exchange.model.core.ib.chart.signals.IbChartSignalOptimizationControllerListener;
 import com.munch.exchange.services.ejb.interfaces.IIBHistoricalDataProvider;
+
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Table;
@@ -32,10 +42,76 @@ import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.ProgressBar;
+import org.moeaframework.analysis.diagnostics.ControllerEvent;
+import org.moeaframework.analysis.diagnostics.ControllerListener;
+import org.moeaframework.analysis.diagnostics.ResultKey;
+import org.moeaframework.analysis.diagnostics.SortedListModel;
+import org.moeaframework.core.Settings;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
-public class SignalOptimizationEditorPart {
+
+public class SignalOptimizationEditorPart implements SelectionListener, 
+IbChartSignalOptimizationControllerListener{
+	
+	
+	
+	
 	
 	private static Logger logger = Logger.getLogger(SignalOptimizationEditorPart.class);
+	
+	
+	private static class ContentProvider implements IStructuredContentProvider {
+		public Object[] getElements(Object inputElement) {
+			if(inputElement instanceof LinkedList){
+				@SuppressWarnings({  "unchecked" })
+				LinkedList<String> list=(LinkedList<String>) inputElement;
+				return list.toArray();
+			}
+			
+			return new Object[0];
+		}
+		public void dispose() {
+		}
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		}
+	}
+	
+	
+	private class TableLabelProvider extends LabelProvider implements ITableLabelProvider {
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
+		}
+		public String getColumnText(Object element, int columnIndex) {
+			
+			if(element instanceof String){
+			
+			switch (columnIndex) {
+			case 0: 
+				return (String) element;
+			case 1: 
+				return String.valueOf(controller.get((String) element).size());
+			
+			default: 
+				return element.toString();
+			}
+			}
+			else{
+				return element.toString();
+			}
+			
+		}
+	}
+	
 	
 	
 	public static final String SIGNAL_OPTIMIZATION_EDITOR_ID="com.munch.exchange.partdescriptor.chart.signal.optimization.editor";
@@ -51,6 +127,28 @@ public class SignalOptimizationEditorPart {
 	
 	LinkedList<LinkedList<IbBar>> backTestingBlocks;
 	LinkedList<LinkedList<IbBar>> optimizationBlocks;
+	
+	LinkedList<String> sortedAlgorithmNames;
+	
+	
+	/**
+	 * The underlying data model storing all available results.
+	 */
+	private LinkedList<String> resultListModel;
+	
+	/**
+	 * The underlying data model storing all available metrics.
+	 */
+	private LinkedList<String> metricListModel;
+	
+	
+	/**
+	 * The controller which stores the underlying data model and notifies this
+	 * diagnostic tool of any changes.
+	 */
+	private IbChartSignalOptimizationController controller;
+	
+	
 	
 	private Combo comboBarSize;
 	private Spinner spinnerPercentOfData;
@@ -81,6 +179,11 @@ public class SignalOptimizationEditorPart {
 	private Button btnClear;
 	private ProgressBar progressBarRun;
 	private Label lblMemory;
+	private Label lblReport;
+	private Combo comboReportType;
+	private TabItem tbtmApproximationSet;
+	private TableColumn tblclmnAlgorithm;
+	private TableColumn tblclmnNbOfSeeds;
 	
 	public SignalOptimizationEditorPart() {
 	}
@@ -90,6 +193,9 @@ public class SignalOptimizationEditorPart {
 	 */
 	@PostConstruct
 	public void createControls(Composite parent) {
+		
+		preGuiInitialization();
+		
 		parent.setLayout(new GridLayout(1, false));
 		
 		compositeMain = new Composite(parent, SWT.NONE);
@@ -111,7 +217,9 @@ public class SignalOptimizationEditorPart {
 		
 		comboBarSize = new Combo(groupControls, SWT.NONE);
 		comboBarSize.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		//comboBarSize.setText(comboBarSize.getItem(0));
+		for(String bSize:IbBar.getAllBarSizesAsString())
+			comboBarSize.add(bSize);
+		comboBarSize.setText(comboBarSize.getItem(0));
 		
 		lblPercentOfData = new Label(groupControls, SWT.NONE);
 		lblPercentOfData.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
@@ -130,6 +238,9 @@ public class SignalOptimizationEditorPart {
 		
 		comboAlgorithm = new Combo(groupControls, SWT.NONE);
 		comboAlgorithm.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		comboAlgorithm.setItems(sortedAlgorithmNames.toArray(new String[0]));
+		comboAlgorithm.select(0);
+		
 		
 		lblSeeds = new Label(groupControls, SWT.NONE);
 		lblSeeds.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
@@ -145,36 +256,99 @@ public class SignalOptimizationEditorPart {
 		spinnerMaxNFE = new Spinner(groupControls, SWT.BORDER);
 		spinnerMaxNFE.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 		
+		lblReport = new Label(groupControls, SWT.NONE);
+		lblReport.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblReport.setText("Report:");
+		
+		comboReportType = new Combo(groupControls, SWT.NONE);
+		comboReportType.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
 		compositeCommandBtns = new Composite(compositeCommand, SWT.NONE);
 		compositeCommandBtns.setLayout(new FillLayout(SWT.HORIZONTAL));
 		compositeCommandBtns.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
 		
 		btnRun = new Button(compositeCommandBtns, SWT.NONE);
+		btnRun.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				logger.info("Click on btnRun");
+			}
+		});
 		btnRun.setText("Run");
 		
 		btnCancel = new Button(compositeCommandBtns, SWT.NONE);
+		btnCancel.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				logger.info("Click on btnCancel");
+			}
+		});
 		btnCancel.setText("Cancel");
 		
 		btnClear = new Button(compositeCommandBtns, SWT.NONE);
+		btnClear.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				logger.info("Click on btnClear");
+			}
+		});
 		btnClear.setText("Clear");
 		
 		grpDisplayedResults = new Group(compositeCommand, SWT.NONE);
-		grpDisplayedResults.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		grpDisplayedResults.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		grpDisplayedResults.setLayout(new GridLayout(1, false));
 		grpDisplayedResults.setText("Displayed Results");
 		
 		tableViewerResults = new TableViewer(grpDisplayedResults, SWT.BORDER | SWT.FULL_SELECTION);
+		tableViewerResults.setLabelProvider(new TableLabelProvider());
+		tableViewerResults.setContentProvider(new ContentProvider());
+		tableViewerResults.setInput(resultListModel);
 		tableResults = tableViewerResults.getTable();
+		tableResults.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+			}
+		});
+		tableResults.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				logger.info("Double click on Table");
+			}
+		});
+		tableResults.setHeaderVisible(true);
 		tableResults.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		
+		
+		tblclmnAlgorithm = new TableColumn(tableResults, SWT.NONE);
+		tblclmnAlgorithm.setWidth(150);
+		tblclmnAlgorithm.setText("Algorithm");
+		
+		tblclmnNbOfSeeds = new TableColumn(tableResults, SWT.LEFT);
+		tblclmnNbOfSeeds.setWidth(100);
+		tblclmnNbOfSeeds.setText("Nb of Seeds");
+		
 		
 		composite = new Composite(grpDisplayedResults, SWT.NONE);
 		composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 		composite.setLayout(new FillLayout(SWT.HORIZONTAL));
 		
 		btnSelectAll = new Button(composite, SWT.NONE);
+		btnSelectAll.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				logger.info("Click on btnSelectAll");
+			}
+		});
 		btnSelectAll.setText("Select All");
 		
 		btnShowStatistic = new Button(composite, SWT.NONE);
+		btnShowStatistic.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				logger.info("Click on btnShowStatistic");
+			}
+		});
 		btnShowStatistic.setText("Show Statistic");
 		
 		grpDisplayedMetrics = new Group(compositeCommand, SWT.NONE);
@@ -184,6 +358,7 @@ public class SignalOptimizationEditorPart {
 		
 		listViewerMetrics = new ListViewer(grpDisplayedMetrics, SWT.BORDER | SWT.V_SCROLL);
 		listMetrics = listViewerMetrics.getList();
+		listMetrics.addSelectionListener(this);
 		listMetrics.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		compositeChart = new Composite(compositeMain, SWT.NONE);
@@ -197,6 +372,9 @@ public class SignalOptimizationEditorPart {
 		TabItem tbtmMetrics = new TabItem(tabFolder, SWT.NONE);
 		tbtmMetrics.setText("Metrics");
 		
+		tbtmApproximationSet = new TabItem(tabFolder, SWT.NONE);
+		tbtmApproximationSet.setText("Approximation Set");
+		
 		Composite compositeBottom = new Composite(parent, SWT.BORDER);
 		compositeBottom.setLayout(new FillLayout(SWT.HORIZONTAL));
 		compositeBottom.setLayoutData(new GridData(SWT.FILL, SWT.BOTTOM, true, false, 1, 1));
@@ -208,11 +386,44 @@ public class SignalOptimizationEditorPart {
 		
 		lblMemory = new Label(compositeBottom, SWT.NONE);
 		lblMemory.setText("Memory");
-		for(String bSize:IbBar.getAllBarSizesAsString())
-			comboBarSize.add(bSize);
 		
+		
+		
+		postGuiInitialization();
 		
 	}
+	
+	
+	private void preGuiInitialization(){
+		
+		
+		controller=new IbChartSignalOptimizationController(signal);
+		controller.addControllerListener(this);
+		
+		resultListModel = new LinkedList<String>();
+		metricListModel = new LinkedList<String>();
+		
+		// initialize the sorted list of algorithms
+		Set<String> algorithmNames = new HashSet<String>();
+
+		for (String algorithm : Settings.getDiagnosticToolAlgorithms()) {
+			algorithmNames.add(algorithm);
+		}
+
+		for (String algorithm : Settings.getPISAAlgorithms()) {
+			algorithmNames.add(algorithm);
+		}
+
+		sortedAlgorithmNames = new LinkedList<String>(algorithmNames);
+		Collections.sort(sortedAlgorithmNames);
+		
+	}
+	
+	
+	private void postGuiInitialization(){
+		
+	}
+	
 	
 	private LinkedList<LinkedList<IbBar>> collectOptimizationBlocks(LinkedList<LinkedList<IbBar>> allBlocks){
 		LinkedList<LinkedList<IbBar>> optBlocks=new LinkedList<>();
@@ -278,5 +489,26 @@ public class SignalOptimizationEditorPart {
 	@Focus
 	public void setFocus() {
 		// TODO	Set the focus to control
+	}
+
+	
+
+	@Override
+	public void controllerStateChanged(
+			IbChartSignalOptimizationControllerEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void widgetSelected(SelectionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void widgetDefaultSelected(SelectionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
