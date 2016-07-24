a83232a781437983e9cc1f23e13eb060b3792fb6
paul-edouard <paul-edouard@192.168.192.52>
2016-07-24 14:24:13 +0200

    open range

---
+++ com.munch.exchange.server.ejb/ejbModule/com/munch/exchange/model/core/ib/chart/signals/strategies/OpenRange_.java
@@ -0,0 +1,10 @@
+package com.munch.exchange.model.core.ib.chart.signals.strategies;
+
+import com.munch.exchange.model.core.ib.chart.signals.IbChartSignal_;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2016-07-24T12:02:17.737+0200")
+@StaticMetamodel(OpenRange.class)
+public class OpenRange_ extends IbChartSignal_ {
+}
